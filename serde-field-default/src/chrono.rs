b54d527409bf4fd8df52f16b7cc304866b85c0f4
vkill <vkill.net@gmail.com>
2022-12-16 20:31:33 +0800

    Clippy

--- serde-field-default/src/chrono.rs
+++ serde-field-default/src/chrono.rs
@@ -3,5 +3,5 @@ pub use chrono;
 use chrono::{DateTime, NaiveDateTime, Utc};
 
 pub fn default_date_time_utc() -> DateTime<Utc> {
-    DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc)
+    DateTime::from_utc(NaiveDateTime::from_timestamp_opt(0, 0).expect(""), Utc)
 }
